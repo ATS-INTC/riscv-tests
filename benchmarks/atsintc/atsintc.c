57f92c5fa3ebed14159468f4a4f81cbddee7bcf1
zflcs <1491657576@qq.com>
2024-03-02 00:50:15 +0800

    update atsintc test

--- benchmarks/atsintc/atsintc.c
+++ benchmarks/atsintc/atsintc.c
@@ -5,13 +5,18 @@ int main( int argc, char* argv[] ) {
     printf("test begin\n");
     uint64_t *read_reg = 0x10000000;
     uint64_t *write_reg = 0x10000008;
+    uint64_t *write_reg1 = 0x10000010;
     int i = 0;
-    for (i = 0; i < 3; i ++) {
+    for (i = 0; i < 5; i ++) {
         *write_reg = 0x19990100 + i;
 		    printf("wait a moment\n");
     }
+    // for (i = 0; i < 2; i ++) {
+    //     *write_reg1 = 0x990109 + i;
+		//     printf("wait a moment\n");
+    // }
     uint64_t res = 0;
-    for (i = 0; i < 3; i ++) {
+    for (i = 0; i < 5; i ++) {
       res = *read_reg;
       printf("a read res 0x%lx\n", res);
     }
